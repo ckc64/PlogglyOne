ebb8c6560b2fd0b1fc3681129a509b22ac77868e
Charles Kurt <32605261+ckc64@users.noreply.github.com>
2019-08-21 19:58:35 +0800

    added

---
+++ ploggly/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'startpages/splashscreen.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+        home: MainApp()
+    );
+  }
+}
+
+class MainApp extends StatefulWidget {
+  @override
+  _MainAppState createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen();
+  }
+}
---
+++ ploggly/lib/startpages/splashscreen.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+import 'login.dart';
+
+
+
+void main() => runApp(SplashScreenApp());
+
+class SplashScreenApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+      
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+ 
+    super.initState();
+    Future.delayed(Duration(
+      seconds: 3
+      ),(){
+
+        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>LoginPage()));
+       
+     },
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.pink,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Center(
+              child:Text('ploggly.',
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'Pacifico',
+                fontSize: 35.0,
+              ),
+
+              ),
+            ),
+            SizedBox(height: 15.0,),
+            Center(
+              child:Text('share yourself to the world',
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'Montserrat',
+                fontSize: 10.0,
+              ),
+
+              ),
+            ),
+          ],
+        ),
+    );
+  }
+}
