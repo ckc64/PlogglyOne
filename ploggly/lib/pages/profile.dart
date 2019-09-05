2ee64254ad792b81604560aa3e2f030dcd1fc360
Charles Kurt <32605261+ckc64@users.noreply.github.com>
2019-09-06 06:12:38 +0800

    updated

--- ploggly/lib/pages/profile.dart
+++ ploggly/lib/pages/profile.dart
@@ -3,6 +3,7 @@ import 'package:ploggly/widgets/header.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:ploggly/widgets/progress.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class Profile extends StatefulWidget {
   final String profileID;
@@ -30,9 +31,106 @@ buildProfileHeader(){
           return circularProgress();
       }
       //final user = userRef.document(widget.profileID).snapshots();
-      return Center(child: Text(snapshot.data['bio'],style: TextStyle(color: Colors.black,fontSize: 35),));
+      return Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(children: <Widget>[
+            Row(children: <Widget>[
+              CircleAvatar(radius: 40.0,
+                backgroundColor: Colors.pink,
+                backgroundImage: CachedNetworkImageProvider(snapshot.data['profpic']),
+              ),
+              Expanded(
+                flex:1,
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        buildCountColumn("posts",0),
+                        buildCountColumn("followers",0),
+                        buildCountColumn("following",0),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                          buildProfileButton()
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.only(top:12.0),
+              child: Text(snapshot.data['username'],
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Montserrat'
+              ),
+              ),
+            ),
+             Container(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.only(top:4.0),
+              child: Text(snapshot.data['name'],
+              style: TextStyle(
+               
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Montserrat'
+              ),
+              ),
+            ),
+             Container(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.only(top:2.0),
+              child: Text(snapshot.data['bio'],
+              style: TextStyle(
+               
+                fontFamily: 'Montserrat'
+              ),
+              ),
+            )
+         ],
+        ),
+      );
     },
   );
+}
+
+Column buildCountColumn(String label, int count){
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    mainAxisSize: MainAxisSize.min,
+    children: <Widget>[
+      Text(
+        count.toString(),
+        style: TextStyle(
+          fontSize:22.0,
+          fontFamily: 'Montserrat',
+        ),
+      ),
+      Container(
+        margin: EdgeInsets.only(top:4.0),
+        child: Text(
+          label,
+          style: TextStyle(
+            color:Colors.grey,
+            fontFamily: 'Montserrat',
+            fontSize: 15,
+          ),
+        ),
+      )
+    ],
+  );
+}
+
+buildProfileButton(){
+  return Text("Profile Button Here");
 }
 
   @override
