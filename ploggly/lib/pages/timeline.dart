d46ce058496ea8058756e686ce359ff447779b9f
Charles Kurt <32605261+ckc64@users.noreply.github.com>
2019-09-19 12:57:12 +0800

    news feed will no display followings post

--- ploggly/lib/pages/timeline.dart
+++ ploggly/lib/pages/timeline.dart
@@ -1,9 +1,17 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:ploggly/pages/post.dart';
+import 'package:ploggly/pages/search.dart' as prefix0;
 import 'package:ploggly/widgets/header.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ploggly/widgets/progress.dart';
+import 'activity_feed.dart';
 import 'home.dart';
 
 class Timeline extends StatefulWidget {
- 
+ final currentUser;
+
+  const Timeline({Key key, this.currentUser}) : super(key: key);
   @override
   _TimelineState createState() => _TimelineState();
   
@@ -11,11 +19,120 @@ class Timeline extends StatefulWidget {
 
 class _TimelineState extends State<Timeline> {
 
+  List<Post> posts;
+List<String> followingList;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getTimeline();
+    getFollowing();
+  }
+
+  getFollowing() async{
+   QuerySnapshot snapshot =  await followingRef
+      .document(loggedInUser.uid)
+      .collection('usersFollowing')
+      .getDocuments();
+
+      setState(() {
+        followingList = snapshot.documents.map((doc)=>doc.documentID).toList();
+        print("List : $followingList");
+      });
+  }
+  getTimeline()async{
+
+    QuerySnapshot snapshot = await timelineRef
+      .document(widget.currentUser)
+      .collection('timelinePosts')
+      .orderBy('timestamp',descending: true)
+      .getDocuments();
+      List <Post> posts = snapshot.documents.map((doc)=>Post.fromDocument(doc)).toList();
+
+      setState(() {
+       this.posts=posts; 
+      });
+  }
+
+  buildTimeline(){
+    if(posts==null){
+      return circularProgress();
+    }else if(posts.isEmpty){
+      //return Text("posts");
+      buildUsersToFollow();
+    }
+    return ListView(children: posts,);
+  }
+
+  buildUsersToFollow(){
+    final userRef = Firestore.instance.collection('users');
+    return StreamBuilder(
+      stream: userRef.orderBy('timestamp',descending:true).limit(30)
+      .snapshots(),
+        builder: (context,snapshot){
+        if(!snapshot.hasData){
+          return circularProgress();
+        }
+    
+        List<Container> searchResults = [];
+        snapshot.data.documents.forEach((doc){
+              // final bool isAuthUser = loggedInUser.uid==doc['userid'];
+              // final bool isFollowing = followingList.contains(doc['userid']);
+              // if(isAuthUser){
+              //   return;
+              // }else if(isFollowing){
+              //   return;
+              // }
+
+                  searchResults.add(
+          Container(
+            color:Colors.pink,
+            child: Column(
+              children: <Widget>[
+                GestureDetector(
+                  onTap: ()=>showProfile(context,profileId: doc['userid']),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.grey,
+                      backgroundImage: CachedNetworkImageProvider(doc['profpic']),
+                    ),
+                    title: Text(doc['name'],
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Montserrat',
+                    ),
+                    ),
+                    subtitle: Text(doc['username'],style: TextStyle(color: Colors.white),),
+                  ),
+                ),
+                Divider(height: 2.0,color: Colors.white54,)
+              ],
+            ),
+          )
+          );
+               
+           
+       
+        });
+        //  print("Current User "+ loggedInUser.uid+ " doc id "+ doc['userid']+"Follwing list ");
+          print("search $searchResults");
+          return ListView(
+            children: searchResults
+        );
+        
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
   
       appBar: header("ploggly"),
+      body: RefreshIndicator(
+        onRefresh: ()=>getTimeline(),
+        child: buildTimeline(),
+      ),
     );
   }
 }
