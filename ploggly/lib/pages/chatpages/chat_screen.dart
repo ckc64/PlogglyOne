13c2c001abe5cc1fd0fad7798e3db31ac4146657
Charles Kurt <32605261+ckc64@users.noreply.github.com>
2019-09-30 11:20:17 +0800

    added chat screen and send messages

---
+++ ploggly/lib/pages/chatpages/chat_screen.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:ploggly/pages/home.dart';
+import 'constants.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class ChatScreen extends StatefulWidget {
+  final profileID;
+
+  const ChatScreen({Key key, this.profileID}) : super(key: key);
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+
+    FirebaseAuth fAuth = FirebaseAuth.instance;
+    //FirebaseUser loggedInUser;
+    TextEditingController messageController = TextEditingController();
+    final messageRef = Firestore.instance.collection('messages');
+    @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print(widget.profileID);
+  }
+
+ 
+
+  getMessages(){
+    messageRef.getDocuments();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: null,
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.close),
+              onPressed: () {
+                Navigator.pop(context);
+              }),
+        ],
+        title: Text('Messages'),
+        backgroundColor: Colors.pink,
+      ),
+      body: SafeArea(
+         
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              decoration: kMessageContainerDecoration,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      controller: messageController,
+                      decoration: kMessageTextFieldDecoration,
+                    ),
+                  ),
+                  FlatButton(
+                    onPressed: (){
+                       messageRef
+
+                            .add({
+                              "sender":loggedInUser.email,
+                              "message":messageController.text,
+                              "receiver":widget.profileID
+                            });
+                    },
+                    child: Text(
+                      'Send',
+                      style: kSendButtonTextStyle,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
